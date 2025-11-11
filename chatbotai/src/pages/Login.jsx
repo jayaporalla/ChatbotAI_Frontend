5cf5ad0a65c956751d5eb7996cedfaa9f53fb60a
Jayasri P <jayaporalla921@gmail.com>
2025-11-11 21:24:10 +0530

    pages added

--- chatbotai/src/pages/Login.jsx
+++ chatbotai/src/pages/Login.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+
   return (
-    <div className="flex justify-center items-center">
-      <form>
-        
+    <div className="flex justify-center items-center h-screen">
+      <form className="bg-white p-6 rounded shadow-md w-full md:w-[500px]" onSubmit={submitHandler}>
+        <h2 className="text-3xl mb-4">Login</h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2 text-xl" htmlFor="email">Email:</label>
+          <input 
+          type="email" 
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border p-2 w-full rounded outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Enter your email..."  
+          required/>
+        </div>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">Submit</button>
       </form>
     </div>
   );
--- chatbotai/src/pages/Verify.jsx
+++ chatbotai/src/pages/Verify.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 const Verify = () => {
+
+  const [otp, setOtp] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(otp);
+  }
+
   return (
-    <div>
-      <h1>Verify Page</h1>
-      <p>Verify your account here.</p>
+    <div className="flex justify-center items-center h-screen">
+      <form className="bg-white p-6 rounded shadow-md w-full md:w-[500px]" onSubmit={submitHandler}>
+        <h2 className="text-3xl mb-4">Verify</h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2 text-xl" htmlFor="otp">OTP:</label>
+          <input 
+          type="otp" 
+          id="otp"
+          value={otp}
+          onChange={(e) => setOtp(e.target.value)}
+          className="border p-2 w-full rounded outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Enter your OTP..."  
+          required/>
+        </div>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">Submit</button>
+      </form>
     </div>
   );
 };
